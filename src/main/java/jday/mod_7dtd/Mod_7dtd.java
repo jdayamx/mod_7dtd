c9b923367022c4efc14f5c952dc97fbdae4eed38
jday <jday@teedex.net>
2023-08-05 20:57:02 +0300

    Added items

--- src/main/java/jday/mod_7dtd/Mod_7dtd.java
+++ src/main/java/jday/mod_7dtd/Mod_7dtd.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Mod_7dtd {
+package jday.mod_7dtd;
+
+import jday.mod_7dtd.block.ModBlocks;
+import jday.mod_7dtd.item.ModItemGroup;
+import jday.mod_7dtd.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Mod_7dtd implements ModInitializer {
+    public static final String MOD_ID = "m7dtd";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+        ModItemGroup.registerItemGroups();
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
+        Mod_7dtd.LOGGER.info("Loading mod");
+    }
 }
--- src/main/java/jday/mod_7dtd/client/Mod_7dtdClient.java
+++ src/main/java/jday/mod_7dtd/client/Mod_7dtdClient.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Mod_7dtdClient {
+package jday.mod_7dtd.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+public class Mod_7dtdClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // some code for client
+        HudRenderCallback.EVENT.register(new HudOverlay());
+    }
 }
